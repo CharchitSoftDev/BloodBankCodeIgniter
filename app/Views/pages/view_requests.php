a53adb7a31f5eec82a8601335a684a19bcbc58a9
Charchit Khandelwal <charchit@newgenpayments.com>
2021-03-01 19:48:41 +0530

    [Bugfix] Customer restriction order request url
    Signed-off-by: Charchit Khandelwal <charchit@newgenpayments.com>

--- app/Views/pages/view_requests.php
+++ app/Views/pages/view_requests.php
@@ -1,6 +1,7 @@
 <html>
 
 <body>
+    <?php if(session()->get('success') && session()->get('account_type')=="restaurant"):?>
     <div class="menu-table m-4">
         <table name="blood-sample" class="table table-bordered">
             <thead class="thead-dark">
@@ -22,6 +23,19 @@
             </tbody>
         </table>
     </div>
+    <?php else:?>
+    <div class="page-wrap d-flex flex-row align-items-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-12 text-center">
+                    <span class="display-1 d-block">404</span>
+                    <div class="mb-4 lead">The page you are looking for was not found.</div>
+                    <a href="/blood/menu/" class="btn btn-info">Back to Home</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php endif; ?>
 </body>
 
 </html>
